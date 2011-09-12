bffdf5f1a34a3824e2ab45d11a33a6d0334412a2
Mauro Marano <gagginaspinnata@gmail.com>
2011-09-12 17:58:05 +0200

    Aggiunto un mínimo di css per distanziare le colonne della tabella e per aggiungere un bordo separatore per le righe.

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?
-	require_once('fcmClassificaDati.js.php');
+	require_once('js/fcmClassificaDati.js.php');
 
 ?>
 
-<table>
-	<tr>
-		<td>Squadra</td>
-		<td>Totale Squadra</td>
-	</tr>
-	<? 
-	
-		for($i=0; $i<=count($arrClassifica); $i++ ){
+<div id="classifica_somma" style="background-color:white;padding:20px;margin-top:50px;">
+	<table>
+		<tr style="font-size:150%;">
+			<td><b>Squadra</b></td>
+			<td><b>Totale Squadra</b></td>
+		</tr>
+		<? 
 		
-			echo "<tr><td>".$arrClassifica[$i][4]."</td><td>".$arrClassifica[$i][23]."</td></tr>";
+			for($i=0; $i<=count($arrClassifica); $i++ ){
+			
+				echo "<tr style=\"font-site:120%;border-bottom:1px dotted;\"><td>".$arrClassifica[$i][4]."</td><td style=\"padding-left:50px;\">".$arrClassifica[$i][23]."</td></tr>";
+			
+			}
 		
-		}
-	
-	?>
-</table>
+		?>
+	</table>
+</div>
